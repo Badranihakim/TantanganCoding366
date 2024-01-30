be442ce48d992b09c33feb5cf3bc8a425b9c2a1c
Hakim Badrani <hakimbadrani05@gmail.com>
2024-01-30 14:57:44 +0700

    day 4/366

---
+++ 4-smash.js
@@ -0,0 +1,7 @@
+// function smash (words) {
+//     return words.join(' ')
+// };
+
+const smash = (words) => words.join(' ')
+
+console.log(smash(["this", "is", "a", "really", "long", "sentence"]));
