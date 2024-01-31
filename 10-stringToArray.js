79e89b4c5fc4e953bbb50af8d535d1c41784db65
Hakim Badrani <hakimbadrani05@gmail.com>
2024-01-31 15:20:14 +0700

    day 10/366 EZ

---
+++ 10-stringToArray.js
@@ -0,0 +1,8 @@
+// function stringToArray(string){
+//     return string.split(' ')
+// }
+
+const stringToArray = (string) => string.split(' ')
+
+console.log(stringToArray("Robin Singh"));
+console.log(string = ("Robin Singh").split(' '));
